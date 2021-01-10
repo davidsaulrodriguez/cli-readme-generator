27d93a34989457d384eb26333a023e2e1c124fe4
David Rodriguez <david@bsdadm.com>
2021-01-09 22:05:35 -0500

    add functionality to render licensee links.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,25 @@ function renderBadges(username, repoName) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'BSD-2-Clause':
+      return `https://choosealicense.com/licenses/bsd-2-clause/`;
+      break;
+    case 'GPLv2':
+      return `https://choosealicense.com/licenses/gpl-2.0/`;
+      break;
+    case 'GPLv3':
+      return `https://choosealicense.com/licenses/gpl-3.0/`;
+      break;
+    case 'MIT':
+      return `https://choosealicense.com/licenses/mit/`;
+      break;
+    default:
+      return ``;
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
